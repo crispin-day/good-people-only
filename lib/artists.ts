19a596ce098570315bab179bd9581092ce3c568e
Dino <dino@goodpeopleonly.com>
2026-04-22 05:16:59 +0000

    fix: replace Benja with square ping pong photo, center crop position

--- lib/artists.ts
+++ lib/artists.ts
@@ -142,7 +142,7 @@ export const ARTISTS: Artist[] = [
     name: 'Benja',
     slug: 'benja',
     imgSrc: '/artists/benja.jpg',
-    imgPosition: '40% 15%',
+    imgPosition: '50% 35%',
     shortBio: 'Artist. Details incoming.',
     genre: 'Indie Rock',
     division: 'Label',
